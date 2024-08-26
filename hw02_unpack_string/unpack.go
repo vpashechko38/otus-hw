8f30a60bafed1d0e025b3599d524971799c95ba4
vpashechko <vpashechko@ozon.ru>
2024-08-26 22:23:24 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,49 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	char := ""
+
+	sb := strings.Builder{}
+
+	for _, ch := range input {
+		// проверяем, число это или нет
+		dig, err := strconv.Atoi(string(ch))
+		if len(char) == 0 && err == nil {
+			return "", ErrInvalidString
+		}
+
+		// нашли число после символа
+		if len(char) != 0 && err == nil {
+			for i := 0; i < dig; i++ {
+				sb.WriteString(char)
+			}
+			char = ""
+			continue
+		}
+
+		// одинокий символ без числа
+		if len(char) != 0 && err != nil {
+			sb.WriteString(char)
+			char = ""
+		}
+
+		// нашли символ, но пока не нашли число
+		if len(char) == 0 && err != nil {
+			char = string(ch)
+		}
+	}
+
+	// одинокий символ без числа в конце
+	if char != "" {
+		sb.WriteString(char)
+	}
+
+	return sb.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,8 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "б2а4", expected: "ббаааа"},
+		{input: "😀4", expected: "😀😀😀😀"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
