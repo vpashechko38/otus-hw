3082f5df0945f8a63a58a588aabbf3e54d391b7f
vpashechko <vpashechko@ozon.ru>
2024-12-28 16:51:57 +0300

    [hw03] Получение топ 10 популярных слов

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,48 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+
+	"github.com/samber/lo"
+)
+
+var reg = "\\s+"
+
+type elem struct {
+	word  string
+	count int
+}
+
+func Top10(input string) []string {
+	re := regexp.MustCompile(reg)
+	words := re.Split(input, -1)
+	dict := make(map[string]int)
+	for _, word := range words {
+		if word == "" {
+			continue
+		}
+
+		dict[word]++
+	}
+
+	elems := make([]elem, 0)
+	for word, count := range dict {
+		elems = append(elems, elem{word, count})
+	}
+
+	sort.Slice(elems, func(i, j int) bool {
+		if elems[i].count == elems[j].count {
+			return elems[i].word < elems[j].word
+		}
+		return elems[i].count > elems[j].count
+	})
+
+	top := lo.Map(elems, func(item elem, _ int) string {
+		return item.word
+	})
+	if len(top) >= 10 {
+		return top[:10]
+	}
+	return top
 }
